ff1befe84d79cd3852af99955aa88d909d6e0836
Andrew <andrewhuth2@live.com>
2014-07-02 15:35:53 -0400

    Require platform/platform first

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,8 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require platform/platform
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require platform/platform
 //= require_tree .
